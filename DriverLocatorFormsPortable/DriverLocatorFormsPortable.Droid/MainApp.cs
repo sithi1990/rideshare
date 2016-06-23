ed42ad33fedbbd81fa64c72cf3fc8420f4a989c6
sithi1990 <sithi1990@live.com>
2016-06-23 14:29:18 +0530

    Integrated Urban Airship and added missed project reffernces

---
+++ DriverLocatorFormsPortable/DriverLocatorFormsPortable.Droid/MainApp.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using UrbanAirship;
+
+namespace DriverLocatorFormsPortable.Droid
+{
+    [Application]
+    public class MainApp : Application
+    {
+        public MainApp(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+
+            // Call takeOff
+            UAirship.TakeOff(this, (UAirship airship) => {
+                UAirship.Shared().PushManager.UserNotificationsEnabled = true;
+            });
+
+            
+        }
+    }
+}
